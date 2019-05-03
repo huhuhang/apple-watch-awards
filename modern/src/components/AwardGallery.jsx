f87a06fa03bfc7a425a421d312d324e71ef6ce44
YangKui <c6swift@gmail.com>
2019-05-03 20:37:57 +0800

    feat: 支持 sticky title

--- modern/src/components/AwardGallery.jsx
+++ modern/src/components/AwardGallery.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import Award from './Award';
+
+const GalleryTitle = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 10;
+
+  h2 {
+    width: 100%:
+  }
+`;
 
 const AwardGallery = ({ title, awards }) => {
   return (
-    <section>
-      <header>
-        <h2 className="button primary fit small">{title}</h2>
-      </header>
-      <section className="tiles">
-        {awards.map(award => (
-          <Award key={award.name} {...award} />
-        ))}
-      </section>
+    <section id="main">
+      <div className="inner">
+        <GalleryTitle>
+          <h2 className="button primary fit small">{title}</h2>
+        </GalleryTitle>
+        <section className="tiles">
+          {awards.map(award => (
+            <Award key={award.name} {...award} />
+          ))}
+        </section>
+      </div>
     </section>
   );
 };
